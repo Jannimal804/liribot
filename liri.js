a8a40eb7a2813bf5960f1c6952d044e187ca5b85
Jannimal804 <jannalhall@gmail.com>
2019-01-29 18:30:46 -0500

    added OMDB movie-this functionality

--- liri.js
+++ liri.js
@@ -11,6 +11,12 @@ var command = process.argv[2];
 if (command === "concert-this") { 
     concertThis();
 }
+else if (command === "spotify-this-song"){
+  spotifyThis();
+}
+else if (command === "movie-this"){
+  movieThis();
+}
 
 //concert-this 
 function concertThis() { 
@@ -24,5 +30,39 @@ axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=cod
     console.log(moment(response.data[0].datetime).format("L"));
   }
 ); 
-    
+}
+
+//spotify-this-song
+function spotifyThis(){
+var song = process.argv[3];
+ 
+spotify.search({ type: 'track', query: song }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+  console.log(data.tracks.items[0].album.artists[0].name);
+  console.log(data.tracks.items[0].name);
+  console.log(data.tracks.items[0].preview_url);
+  console.log(data.tracks.items[0].album.name);
+});
+
+}
+
+//movie-this
+function movieThis(){
+ var movie = process.argv[3];
+
+ axios.get("https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+   function(response) {
+     console.log(response.data.Title);
+     console.log(response.data.Year);
+     console.log(response.data.Ratings[1]);
+     console.log(response.data.Country);
+     console.log(response.data.Language);
+     console.log(response.data.Plot);
+     console.log(response.data.Actors);
+   }
+ )
+
 }
